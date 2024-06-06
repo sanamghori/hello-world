f269b467dc9d8a1602ca5cb9a8a54ebda3006f52
sanamghori <161497237+sanamghori@users.noreply.github.com>
2024-06-06 21:56:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("Hello world");
+var Nam = "jhon doe";
+var age = 22;
+var Admin = true;
+console.log(Nam);
+console.log(age);
+console.log(Admin);
+var firstName = "jhon";
+var lastName = "doe";
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+var num = 10;
+var start = "Hello";
+var bool = true;
+console.log(num);
+console.log(start);
+console.log(bool);
---
+++ main.ts
@@ -0,0 +1,24 @@
+console.log("Hello world");
+
+let Nam:string="jhon doe";
+let age:number=22;
+let Admin:boolean=true;
+console.log(Nam);
+console.log(age);
+console.log(Admin);
+
+
+let firstName:string="jhon";
+let lastName:string="doe";
+let fullName:string= firstName +' '+ lastName;
+console.log(fullName);
+
+
+let num:number=10;
+let start:string="Hello";
+let bool:boolean=true
+console.log(num);
+console.log(start);
+console.log(bool);
+
+
